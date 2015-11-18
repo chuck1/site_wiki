88cc7e047e1d6876c2b8ea3608520036a995546c
Charles Rymal <charlesrymal@gmail.com>
2015-11-17 17:16:56 -0800

    add process_data; move functions from export command to util

--- wiki/management/commands/process_data.py
+++ wiki/management/commands/process_data.py
@@ -1,3 +1,4 @@
+import django.core.management
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
@@ -21,11 +22,32 @@ class Command(BaseCommand):
 		recursivly descend tree and add all page html to a single string
 		'''
 		
-		g = wiki.util.FileListGenerator()
-			
+		def filt(x):
+			s = x[-8:]
+			assert len(s) == len('data.txt')
+			if s == 'data.txt':
+				return True
+			return False
+		
+		g = wiki.util.FileListGenerator(filt)
+		
 		file_list = g()
 		
 		for f in file_list:
-				print f
+			print f
+			
+			dir = os.path.dirname(f)
+			
+			j = wiki.util.get_data(f)
+			
+			print j
+			
+			if 'python_doc' in j:
+				lst = j['python_doc']
+				for l in lst:
+					django.core.management.call_command('python_doc', l[0],
+						os.path.join(dir, l[1]))
+			
+			
 
-
+		
--- wiki/util.py
+++ wiki/util.py
@@ -11,6 +11,12 @@ def list_data_src():
 				path = os.path.join(root, f)
 				print path
 
+def get_data(f):
+	f = os.path.join(settings.WIKI_SRC_ROOT, f)
+	with open(f, 'r') as f:
+		s = f.read()
+	return json.loads(s)
+
 def get_data_dir(dir):
 	path = os.path.join(settings.WIKI_SRC_ROOT, dir, 'data.txt')
 	if not os.path.exists(path): return None
@@ -99,4 +105,86 @@ def child_link_html(dir):
 	html = markdown.markdown(raw)
 	return html
 
+def listize(p):
+    h,t = os.path.split(p)
+    l = []
+    while h:
+        #print 'h',h,'t',t
+        l.insert(0,h)
+        h,t = os.path.split(t)
+    l.append(t)
+    return l
+
+def get_node(lst, d):
+    while lst:
+        l = lst.pop(0)
+        try:
+            d = d[0][l]
+        except KeyError:
+            d[0][l] = [dict(),[]]
+            d = d[0][l]
+    return d
+
+def file_cmp(x,y):
+    if (x[:5] == 'index') and (y[:5] != 'index'): return True
+    if (x[:5] != 'index') and (y[:5] == 'index'): return False
+    return cmp(x,y)
+
+class FileListGenerator(object):
 	
+	def __init__(self, filter_func = None):
+		self.filter_func = filter_func
+	
+	def recurse(self, p, tree):
+		#print 'tree',tree
+		#print 'p',p
+
+		for f in sorted(tree[1], cmp=file_cmp):
+			self.file_list.append(os.path.join(*(p + [f])))
+		
+		for key in sorted(tree[0].keys()):
+			self.recurse(p + [key], tree[0][key])
+		
+	def __call__(self):
+	
+		self.file_list = []
+		
+		tree = [{},[]]
+		
+		for root, dirs, files in os.walk(settings.WIKI_SRC_ROOT):
+			#print 'root',root
+			#print 'dirs',dirs
+			root = os.path.relpath(root, settings.WIKI_SRC_ROOT)
+			
+			#h,t = os.path.split(root)
+			#print repr(h),repr(t),repr(root)
+			if root[:4] != '.git':
+				#print root
+				#print files
+				l = listize(root)
+				#print l
+				n = get_node(l, tree)
+				n[1] = files
+		
+		self.recurse([], tree)
+		
+		def file_list_cmp(p0,p1):
+			h0, t0 = os.path.split(p0)
+			h1, t1 = os.path.split(p1)
+			
+			if h0 != h1:
+				return cmp(h0, h1)
+			
+			return file_cmp(t0,t1)
+		
+		self.file_list = sorted(self.file_list, cmp=file_list_cmp)
+		
+		# filter
+		if self.filter_func is not None:
+			self.file_list = filter(self.filter_func, self.file_list)
+		
+		return self.file_list
+
+
+
+
