f046d04a6d70e521c9dab08088c6a4f2f33764ce
chuck <charlesrymal@gmail.com>
2015-12-13 00:41:31 +0000

    dont want to ignore local settings
    make local settings depend on socket.gethostname()

---
+++ site_wiki/settings_local_raspberrypi.py
@@ -0,0 +1,6 @@
+HOME = "/home/chuck/"
+WIKI_SOURCE_DIR		= HOME+'git/site_wiki_source'
+WIKIi_BUILD_DIR		= HOME+'.build/site_wiki/build'
+WIKI_SEMISTATIC_DIR	= HOME+'.build/site_wiki/semistatic'
+WIKI_STATIC_DIR 	= HOME+'media'
+
