1c5427407b7b301a14ee6c780ee6d142afb4045f
crymal <charles.rymal@nortek.com>
2016-04-07 19:00:48 -0700

    auto commit

--- task/forms.py
+++ task/forms.py
@@ -7,7 +7,7 @@ class TaskEditForm(forms.Form):
 	notes   = forms.CharField(label = 'notes', widget=forms.Textarea)
         priority = forms.IntegerField(label = "priority")
         bool_wait_for_feedback = forms.BooleanField(label = "wait for feedback", required=False)
-        parent = forms.ModelChoiceField(queryset=task.models.Task.objects.all(), empty_label="None")
+        parent = forms.ModelChoiceField(queryset=task.models.Task.objects.all(), empty_label="None", required=False)
 
 class TaskCreateForm(forms.Form):
 	name = forms.CharField(label = 'name', max_length = 256)
